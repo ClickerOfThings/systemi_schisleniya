e2126bc2503419dcfe2ec38f30d4a409b937ba87
ClickerOfThings <clickerofthings@gmail.com>
2018-10-17 23:19:48 +0400

    glaza bolyat aaaaa

--- formulas.py
+++ formulas.py
@@ -58,3 +58,42 @@ def vosemTo10(num):
     print("Итоговое десятичное число: " + str(desyat))
     print("\n-------------------------------------\n")
     return desyat
+
+# -------------
+
+def vosemTo16(num):
+    error = "error"
+    noerror = "noerror"
+    for x in num:
+        if (x in """89.,;:?!*+=- !@#$%^&*()_+-=~`[]йцукенгшщзхъфывапролджэячсмитьбюqwertyuiopasdfghjkl;zxcvbnm,./\\
+            ЙЦУКЕНГШЩЗХЪФЫВАПРОЛДЖЭЯЧСМИТЬБЮQWERTYUIOPASDFGHJKLZXCVBNM"""):
+            # как мне еще проверять наличие символов /\/\ ? внезапный ерор никому не нужен
+            return error
+    rezultat = []
+    desyat = vosemTo10(num)
+    print("Перевод из десятичной СС в шестнадцатиричную:\n")
+    while desyat != 0:
+        x = desyat % 16
+        print("Берем " + str(desyat) + ", делим на 16. Остаток " + str(x) + ". ", end="")
+        if (x == 10 or x == 11 or x == 12 or x == 13 or x == 14 or x == 15):
+            y = x
+            y = str(y)
+            y = y.replace("10", "A")
+            y = y.replace("11", "B")
+            y = y.replace("12", "C")
+            y = y.replace("13", "D")
+            y = y.replace("14", "E")
+            y = y.replace("15", "F")
+            # print (str(y)+" debugdebug") #debug
+            print("Числа больше 9 заменяются на символы от A до F соответсвенно. В данном случае " + str(x) + " заменяется на "+str(y)+". ", end="")
+            x = y
+        rezultat.append(x)
+        # print ("остаток "+str(x)) #debug
+        desyat = desyat // 16
+        print("Остается " + str(desyat))
+    vrem = "".join(str(s) for s in rezultat)
+    print("")
+    print("В итоге получается " + str(vrem) + ". Нам это число нужно перевернуть.")
+    rezultat.reverse()
+    print("\n-------------------------------------\n")
+    return rezultat
---
+++ from8to16.py
@@ -0,0 +1,12 @@
+# -*-coding:utf8;-*-
+#qpy:3
+#qpy:console
+
+import formulas as syschis
+
+print ("Офлайн перевод из 8-иричной в двоичную системы счисления.")
+number = input("Введите 16-иричное число: \n")
+print ("\n-------------------------------------\n")
+rezul = syschis.vosemTo16(number)
+rezult = "".join(str(s) for s in rezul)
+print ("Результат: "+rezult)
