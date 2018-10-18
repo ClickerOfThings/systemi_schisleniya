b94f6937e710ae9bd06606908574b2cafb7ffef4
ClickerOfThings <clickerofthings@gmail.com>
2018-10-18 11:15:15 +0400

    ya sovsem debil

--- from8to16.py
+++ from8to16.py
@@ -4,8 +4,8 @@
 
 import formulas as syschis
 
-print ("Офлайн перевод из 8-иричной в двоичную системы счисления.")
-number = input("Введите 16-иричное число: \n")
+print ("Офлайн перевод из 8-иричной в шестнадцатиричную системы счисления.")
+number = input("Введите 8-иричное число: \n")
 print ("\n-------------------------------------\n")
 rezul = syschis.vosemTo16(number)
 rezult = "".join(str(s) for s in rezul)
